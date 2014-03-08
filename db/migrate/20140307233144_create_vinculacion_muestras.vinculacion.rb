0c4b76f5c982d47bedd8e61b7e527015e376eca3
Jonathan Hernández <ion@cimav.edu.mx>
2014-03-07 17:36:50 -0700

    Nuevas tablas

---
+++ db/migrate/20140307233144_create_vinculacion_muestras.vinculacion.rb
@@ -0,0 +1,16 @@
+# This migration comes from vinculacion (originally 20140307203504)
+class CreateVinculacionMuestras < ActiveRecord::Migration
+  def change
+    create_table :vinculacion_muestras do |t|
+      t.references :solicitud
+      t.integer    :consecutivo
+      t.string     :codigo, :limit => 20
+      t.string     :identificacion
+      t.text       :descripcion
+      t.integer    :cantidad
+      t.references :usuario
+      t.string :status, :default => 1 
+      t.timestamps
+    end
+  end
+end
