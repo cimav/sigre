37df878c35e87b2192fe760a85e7f99bf0f967d3
Jonathan Hernández <ion@suavizado.com>
2014-03-09 20:14:42 -0700

    Use app controller

--- apps/rh/app/assets/javascripts/rh/controllers/empleado_controller.js
+++ apps/rh/app/assets/javascripts/rh/controllers/empleado_controller.js
@@ -1,12 +1,6 @@
 App.EmpleadoController = Ember.ObjectController.extend({
   needs: ["application"],
-  relationshipChanged: false,
   isNotDirty: Ember.computed.not('isDirty'),
-  init: function() {
-    this.set('sedesCache', this.get('controllers.application.sedesCache'));
-    this.set('departamentosCache', this.get('controllers.application.departamentosCache'));
-    this.set('empleadosCache', this.get('controllers.application.empleadosCache'));
-  },
   SedeChanged: function(empleado) {
     console.log('Sede cambio');
   }.observes('sede')
