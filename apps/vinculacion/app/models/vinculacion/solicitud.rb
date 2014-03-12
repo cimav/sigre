7cf4fae2c002adedacc25c8a7325757be3accd34
Jonathan Hernández <ion@suavizado.com>
2014-03-11 23:22:20 -0700

    Set consecutivo

--- apps/vinculacion/app/models/vinculacion/solicitud.rb
+++ apps/vinculacion/app/models/vinculacion/solicitud.rb
@@ -1,5 +1,22 @@
 module Vinculacion
   class Solicitud < ActiveRecord::Base
   	has_many :muestras
+
+  	after_create :set_consecutivo
+
+  	def set_consecutivo
+  	  con = Solicitud.where("EXTRACT(YEAR FROM created_at) = :year", {:year => Date.today.year}).maximum('consecutivo')
+      if con.nil?
+        con = 1
+      else
+        con += 1
+      end
+      consecutivo = "%04d" % con
+      self.consecutivo = con
+      year = Date.today.year.to_s.last(2)
+      self.codigo = "#{year}/#{consecutivo}"
+      self.save(:validate => false)
+  	end
+
   end
 end
