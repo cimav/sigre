6b0ca1735325011ac5fae0bb7824a37d4a6e4a94
Jonathan Hernández <ion@cimav.edu.mx>
2014-03-14 18:58:07 -0700

    Embed ids

--- apps/vinculacion/app/serializers/vinculacion/muestra_serializer.rb
+++ apps/vinculacion/app/serializers/vinculacion/muestra_serializer.rb
@@ -1,6 +1,8 @@
 module Vinculacion
   class MuestraSerializer < ActiveModel::Serializer
+  	embed :ids, :include => true
+    attributes :id
   	attributes :solicitud_id, :usuario_id
-    attributes :id, :consecutivo, :codigo, :identificacion, :descripcion, :cantidad, :status
+    attributes :consecutivo, :codigo, :identificacion, :descripcion, :cantidad, :status
   end
 end
--- apps/vinculacion/app/serializers/vinculacion/solicitud_serializer.rb
+++ apps/vinculacion/app/serializers/vinculacion/solicitud_serializer.rb
@@ -1,7 +1,9 @@
 module Vinculacion
   class SolicitudSerializer < ActiveModel::Serializer
+  	embed :ids, :include => true
+    attributes :id
     attributes :proyecto_id, :sede_id, :cliente_id, :contacto_id, :usuario_id
-    attributes :id, :consecutivo, :codigo, :tipo, :prioridad, :contacto_email, :notas, :acuerdos, :status
+    attributes :consecutivo, :codigo, :tipo, :prioridad, :contacto_email, :notas, :acuerdos, :status
     has_many :muestras
   end
 end
