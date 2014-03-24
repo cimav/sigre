ea91759e22044abd07bd848773fd63185047221e
Jonathan Hernández <ion@suavizado.com>
2014-03-23 22:53:29 -0700

    Limpiar código

--- apps/vinculacion/app/assets/javascripts/vinculacion/controllers/servicio/edit_controller.js
+++ apps/vinculacion/app/assets/javascripts/vinculacion/controllers/servicio/edit_controller.js
@@ -10,24 +10,21 @@ App.ServicioEditController = Ember.ObjectController.extend({
 });
 
 App.EditChildController = Ember.ObjectController.extend({    
-    selected: function() {
-        var muestra = this.get('content');
-        var muestras = this.get('parentController.muestras');
-        return muestras.contains(muestra);
-    }.property(),
-    selectedChanged: function() {
-        console.log('EDIT selected changed');
-        var muestra = this.get('content');
-        var servicio = this.get('parentController').get('model');
-        var muestras = this.get('parentController.muestras');
-        if (this.get('selected')) {                                    
-          muestras.pushObject(muestra);          
-        } else {                                    
-          muestras.removeObject(muestra);
-        }        
-        
-        var selected_muestras = muestras.map(function(el) { return el.id}).toArray().join();
-        servicio.set('muestras_string', selected_muestras);
-
-    }.observes('selected')
+  selected: function() {
+    var muestra = this.get('content');
+    var muestras = this.get('parentController.muestras');
+    return muestras.contains(muestra);
+  }.property(),
+  selectedChanged: function() {
+    var muestra = this.get('content');
+    var servicio = this.get('parentController').get('model');
+    var muestras = this.get('parentController.muestras');
+    if (this.get('selected')) {                                    
+      muestras.pushObject(muestra);          
+    } else {                                    
+      muestras.removeObject(muestra);
+    }        
+    var selected_muestras = muestras.map(function(el) { return el.id}).toArray().join();
+    servicio.set('muestras_string', selected_muestras);
+  }.observes('selected') 
 });
