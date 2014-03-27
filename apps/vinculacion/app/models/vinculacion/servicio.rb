787eaf1c7b877a6bc3b1a9832f37a7333650bc34
Jonathan Hernández <ion@cimav.edu.mx>
2014-03-27 13:35:05 -0700

    Agregar constantes de status

--- apps/vinculacion/app/models/vinculacion/servicio.rb
+++ apps/vinculacion/app/models/vinculacion/servicio.rb
@@ -1,9 +1,30 @@
+# coding: utf-8
 module Vinculacion
   class Servicio < ActiveRecord::Base
   	belongs_to :solicitud
   	has_and_belongs_to_many :muestras, :join_table => :vinculacion_servicios_muestras
 
     after_create :set_extra
+
+    INICIAL          = 1
+    ESPERANDO_COSTEO = 2
+    EN_COTIZACION    = 3
+    EN_PROCESO       = 4
+    FINALIZADO       = 5
+    CANCELADO        = 99
+
+    STATUS = {
+      INICIAL          => 'Inicio',
+      ESPERANDO_COSTEO => 'Esperando costeo',
+      EN_COTIZACION    => 'En cotización',
+      EN_PROCESO       => 'En proceso',
+      FINALIZADO       => 'Finalizado',
+      CANCELADO        => 'Cancelado',
+    }
+
+    def status_text
+      STATUS[status.to_i]
+    end
 
     def set_extra
       con = Servicio.where(:solicitud_id => self.solicitud_id).maximum('consecutivo')
--- apps/vinculacion/app/serializers/vinculacion/servicio_serializer.rb
+++ apps/vinculacion/app/serializers/vinculacion/servicio_serializer.rb
@@ -3,6 +3,7 @@ module Vinculacion
   	embed :ids, :include => true
     attributes :id
     attributes :nombre, :descripcion, :empleado_id, :consecutivo, :codigo, :status
+    attributes :status_text
     has_many :muestras
   end
 end
