8c31b297b1ef61bc9c7e60f6dba29c9859c89414
Jonathan Hernández <ion@cimav.edu.mx>
2014-04-01 16:25:22 -0700

    Quitar acción addMuestra

--- apps/vinculacion/app/assets/javascripts/vinculacion/controllers/solicitud_controller.js
+++ apps/vinculacion/app/assets/javascripts/vinculacion/controllers/solicitud_controller.js
@@ -7,21 +7,5 @@ App.SolicitudController = Ember.ObjectController.extend({
     submit: function() {
       this.get('model').save();
       this.get('controllers.application').notify('Solicitud actualizada');
-    },
-    addMuestra: function() {
-      var muestra = this.get('newMuestra');
-      var self = this
-      var onSuccess = function(muestra) {
-        self.transitionToRoute('solicitud', muestra.get('solicitud').id);
-        self.get('controllers.application').notify('Se agrego nueva muestra');
-        self.set('newMuestra', self.store.createRecord('muestra'));
-      };
-
-      var onFail = function(muestra) {
-        self.get('controllers.application').notify('Error al agregar muestra', 'alert-danger');
-      };
-      this.get('model').get('muestras').pushObject(muestra);
-      muestra.save().then(onSuccess, onFail);
     }
-  }
 });
