3b13d2f662bd532808b2c87915d91104a6692d3d
Jonathan Hernández <ion@cimav.edu.mx>
2014-03-31 18:41:23 -0700

    Mover edición a itemController, WIP. TODO: Rollback / interacciones UI

--- apps/vinculacion/app/assets/javascripts/vinculacion/controllers/muestra_controller.js
+++ apps/vinculacion/app/assets/javascripts/vinculacion/controllers/muestra_controller.js
@@ -1,3 +1,48 @@
 App.MuestraController = Ember.ObjectController.extend({
-  isNotDirty: Ember.computed.not('content.isDirty')
+  needs: ["application", "solicitud"],
+  isNotDirty: Ember.computed.not('content.isDirty'),
+  actions: {
+    editMuestra: function(muestra) {
+      // if(!this.closeEdit()) { return false; }
+      // this.closeNewMuestra();
+      muestra_id = muestra.get('id');
+
+      $('#muestra_' + muestra_id + ' .muestra-info').fadeOut(100);
+      $('#muestra_' + muestra_id).animate({width: "450px"}, 200, function() {
+        $('#muestra_' + muestra_id).addClass('muestra-editing');
+        $('#muestra_' + muestra_id + ' .muestra-edit-form').fadeIn(100);
+      });
+    },
+    updateMuestra: function(muestra) {
+      var self = this
+      var onSuccess = function(muestra) {
+        self.get('controllers.application').notify('Muestra actualizada');
+        $('#muestra_' + muestra.get('id') + ' .muestra-edit-form').fadeOut(100);
+        $('#muestra_' + muestra.get('id')).animate({width: "200px"}, 200, function() {
+          $('#muestra_' + muestra_id).removeClass('muestra-editing');
+          $('#muestra_' + muestra.get('id') + ' .muestra-info').fadeIn(100);
+        });
+      };
+
+      var onFail = function(muestra) {
+        self.get('controllers.application').notify('Error al actualizar muestra', 'alert-danger');
+      };
+      muestra.save().then(onSuccess, onFail);
+    },
+    deleteMuestra: function(muestra) {
+      var app = this.get('controllers.application');
+      if (confirm("¿Desea eliminar la muestra " + muestra.get('codigo') + "?")) {
+        var onSuccess = function (muestra) {
+          app.notify('Se eliminó muestra');
+        };
+        var onFail = function (muestra) {
+          app.notify('Error al eliminar muestra', 'alert-danger');
+        };
+        $('#muestra_' + muestra.get('id')).fadeOut(200, function() {
+          muestra.deleteRecord();
+          muestra.save().then(onSuccess, onFail);
+        });
+      }
+    }
+  }
 });
--- apps/vinculacion/app/assets/javascripts/vinculacion/controllers/muestras_controller.js
+++ apps/vinculacion/app/assets/javascripts/vinculacion/controllers/muestras_controller.js
@@ -1,5 +1,7 @@
 App.MuestrasController = Ember.ArrayController.extend({
   needs: ["application", "solicitud"],
+  itemController: 'muestra',
+
   closeNewMuestra: function() {
     $('#nueva-muestra form').fadeOut(100);
       $('#nueva-muestra').animate({width: "200px"}, 200, function() {
@@ -24,32 +26,6 @@ App.MuestrasController = Ember.ArrayController.extend({
     return result;
   },
   actions: {
-    editMuestra: function(muestra_id) {
-      if(!this.closeEdit()) { return false; }
-      this.closeNewMuestra();
-
-      $('#muestra_' + muestra_id + ' .muestra-info').fadeOut(100);
-      $('#muestra_' + muestra_id).animate({width: "450px"}, 200, function() {
-        $('#muestra_' + muestra_id).addClass('muestra-editing');
-        $('#muestra_' + muestra_id + ' .muestra-edit-form').fadeIn(100);
-      });
-    },
-    updateMuestra: function(muestra) {
-      var self = this
-      var onSuccess = function(muestra) {
-        self.get('controllers.application').notify('Muestra actualizada');
-        $('#muestra_' + muestra.get('id') + ' .muestra-edit-form').fadeOut(100);
-        $('#muestra_' + muestra.get('id')).animate({width: "200px"}, 200, function() {
-          $('#muestra_' + muestra_id).removeClass('muestra-editing');
-          $('#muestra_' + muestra.get('id') + ' .muestra-info').fadeIn(100);
-        });
-      };
-
-      var onFail = function(muestra) {
-        self.get('controllers.application').notify('Error al actualizar muestra', 'alert-danger');
-      };
-      muestra.save().then(onSuccess, onFail);
-    },
     addMuestra: function() {
       var muestra = this.get('newMuestra');
       var self = this
@@ -71,21 +47,6 @@ App.MuestrasController = Ember.ArrayController.extend({
       $('#nueva-muestra').animate({width: "450px"}, 200, function() {
         $('#nueva-muestra form').fadeIn(100);
       });
-    },
-    deleteMuestra: function(muestra) {
-      var self=this
-      if (confirm("¿Desea eliminar la muestra " + muestra.get('codigo') + "?")) {
-        var onSuccess = function (muestra) {
-          self.get('controllers.application').notify('Se eliminó muestra');
-        };
-        var onFail = function (muestra) {
-          self.get('controllers.application').notify('Error al eliminar muestra', 'alert-danger');
-        };
-        $('#muestra_' + muestra.get('id')).fadeOut(200, function() {
-          muestra.deleteRecord();
-          muestra.save().then(onSuccess, onFail);
-        });
-      }
     }
   }
 });
