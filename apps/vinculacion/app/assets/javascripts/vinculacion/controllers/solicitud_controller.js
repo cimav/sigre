a774966d92d1bb7870800b9547b2f92199be9ffe
Jonathan Hernández <ion@suavizado.com>
2014-04-03 12:05:11 -0700

    Mostrar prioridad_string

--- apps/vinculacion/app/assets/javascripts/vinculacion/controllers/solicitud_controller.js
+++ apps/vinculacion/app/assets/javascripts/vinculacion/controllers/solicitud_controller.js
@@ -3,6 +3,13 @@ App.SolicitudController = Ember.ObjectController.extend({
   isNotDirty: Ember.computed.not('content.isDirty'),
   muestrasCount: Ember.computed.alias('content.muestras.length'),
   serviciosCount: Ember.computed.alias('content.servicios.length'),
+  prioridad_string: function() {
+    self = this
+    prioridad = $.grep(this.get('controllers.application.prioridades'), function (p) {
+      return p.id == self.get('prioridad');
+    });
+    return (prioridad = prioridad[0])? prioridad.descripcion : prioridad_txt = 'Indefinida';
+  }.property('prioridad'),
   actions: {
     submit: function() {
       this.get('model').save();
