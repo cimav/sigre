b6fa0c2f0e9cc6d388214e687dc03ed7dd60ec1f
Jonathan Hernández <ion@suavizado.com>
2014-04-07 18:41:39 -0600

    Quitar mail_to de model y agregar helper

---
+++ apps/vinculacion/app/assets/javascripts/vinculacion/helpers/mail_to.js
@@ -0,0 +1,7 @@
+Ember.Handlebars.registerBoundHelper('mailTo', function (emailAddress, label) {
+    emailAddress = Em.Handlebars.Utils.escapeExpression(emailAddress);
+    label = (arguments.length == 2) ? emailAddress : Em.Handlebars.Utils.escapeExpression(label);
+
+    var link = '<a href="mailto:' + emailAddress + '">' + label + '</a>';
+    return new Em.Handlebars.SafeString(link);
+});
--- apps/vinculacion/app/assets/javascripts/vinculacion/models/empleado.js
+++ apps/vinculacion/app/assets/javascripts/vinculacion/models/empleado.js
@@ -5,11 +5,5 @@ App.Empleado = DS.Model.extend({
   email:             DS.attr('string'),
   telefono:          DS.attr('string'),
   nombre_completo:   DS.attr('string'),
-  avatar_url:   DS.attr('string'),
-  mailto:  function() {
-    return 'mailto:' + this.get('email');
-  }.property('email')
-  //sede:              DS.belongsTo('sede'),
-  //departamento:      DS.belongsTo('departamento'),
-  //empleado:          DS.belongsTo('empleado'),
+  avatar_url:        DS.attr('string')
 });
