3ee235ed6ec68126d5d6799395fcd67e25463321
Juan Calderon <juan.calderon@cimav.edu.mx>
2014-05-01 11:22:27 -0600

    App.Proyecto con validaciones.

--- apps/vinculacion/app/assets/javascripts/vinculacion/models/proyecto.js
+++ apps/vinculacion/app/assets/javascripts/vinculacion/models/proyecto.js
@@ -1,5 +1,11 @@
 App.Proyecto = DS.Model.extend(Ember.Validations.Mixin, {
   codigo: DS.attr('string'),
   nombre: DS.attr('string'),
-  solicitudes: DS.hasMany('solicitud')
+  solicitudes: DS.hasMany('solicitud'),
+
+  validations: {
+    codigo: { presence: {message: 'requerido'}},
+    nombre: { presence: {message: 'requerido'}}
+  }
+
 });
