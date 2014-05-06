b4433a64e9896da403a761631df44ae905259dce
Juan Calderon <juan.calderon@cimav.edu.mx>
2014-05-06 14:44:10 -0600

    Se agregaron métodos para obtener núm de elementos de muestras y servicios y la letra de la última cotizacion en SolicitudBusqueda. Se borro sedes (no se usa).

--- apps/vinculacion/app/serializers/vinculacion/solicitud_busqueda_serializer.rb
+++ apps/vinculacion/app/serializers/vinculacion/solicitud_busqueda_serializer.rb
@@ -2,6 +2,6 @@ module Vinculacion
   class SolicitudBusquedaSerializer < ActiveModel::Serializer
     attributes :id
     attributes :codigo, :descripcion, :prioridad
-    attributes :cliente_razon_social, :contacto_nombre, :proyecto_codigo, :proyecto_nombre
+    attributes :cliente_razon_social, :contacto_nombre, :proyecto_codigo, :proyecto_nombre, :muestras_length, :servicios_length, :ultima_cotizacion
   end
 end
