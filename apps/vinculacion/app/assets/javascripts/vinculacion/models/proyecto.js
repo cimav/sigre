aa46e429dc00dc8a0736665a02756c9f3c8158d0
Jonathan Hernández <ion@suavizado.com>
2014-05-06 18:50:04 -0600

    Cargar async las solicitudes del proyecto...

--- apps/vinculacion/app/assets/javascripts/vinculacion/models/proyecto.js
+++ apps/vinculacion/app/assets/javascripts/vinculacion/models/proyecto.js
@@ -1,7 +1,7 @@
 App.Proyecto = DS.Model.extend(Ember.Validations.Mixin, {
   codigo: DS.attr('string'),
   nombre: DS.attr('string'),
-  solicitudes: DS.hasMany('solicitud'),
+  solicitudes: DS.hasMany('solicitud', {async: true}),
 
   validations: {
     codigo: { presence: {message: 'requerido'}},
