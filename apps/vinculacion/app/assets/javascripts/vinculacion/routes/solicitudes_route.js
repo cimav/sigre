7fdc50e8bd9957ce9d40d8b128d4e47cfaac2406
Juan Calderon <juan.calderon@cimav.edu.mx>
2014-05-08 16:25:25 -0600

    Después de guardar una nueva solicitud, recarga la lista de solicitud_busqueda para que incluya la nueva. Se eliminó código innecesario.

--- apps/vinculacion/app/assets/javascripts/vinculacion/routes/solicitudes_route.js
+++ apps/vinculacion/app/assets/javascripts/vinculacion/routes/solicitudes_route.js
@@ -1,21 +1,31 @@
 App.SolicitudesRoute = Ember.Route.extend({
   model: function() {
-    this.store.find('solicitudBusqueda');
-    return this.store.filter(App.SolicitudBusqueda, function(solicitud) {
-      return !solicitud.get('isNew');
-    });
+    return this.store.find('solicitudBusqueda');
+//    return this.store.filter(App.SolicitudBusqueda, function(solicitud) {
+//      return !solicitud.get('isNew');
+//    });
   },
+
+  actions: {
+    reload: function () {
+      var controller = this.controller;
+      this.store.find('solicitudBusqueda').then(function (solicitudes) {
+        controller.set('content', solicitudes);
+      });
+    }
+  },
+
   renderTemplate: function() {
     // FIXME: Corregir bug de back
     this.render({ into: 'application' });
-  },
-  setupController: function (controller, model) {
-    controller.set('content', model);
-  },
-  actions: {
-    delete: function(solicitud) {
-      solicitud.destroyRecord();
-    }
   }
+//  setupController: function (controller, model) {
+//    controller.set('content', model);
+//  },
+//  actions: {
+//    delete: function(solicitud) {
+//      solicitud.destroyRecord();
+//    }
+//  }
 });
 
