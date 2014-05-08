e0171f397a7de220494ab07d4642c88b0ec663a3
Juan Calderon <juan.calderon@cimav.edu.mx>
2014-05-08 10:15:43 -0600

    Proyectos con todos sus campos y layout mejorado. Como mejora, faltaría usar al moment para calcular la diferencia entre fechas.

--- apps/vinculacion/app/assets/javascripts/vinculacion/models/proyecto.js
+++ apps/vinculacion/app/assets/javascripts/vinculacion/models/proyecto.js
@@ -1,6 +1,15 @@
 App.Proyecto = DS.Model.extend(Ember.Validations.Mixin, {
   codigo: DS.attr('string'),
   nombre: DS.attr('string'),
+  descripcion: DS.attr('string'),
+  obj_proyecto: DS.attr('string'),
+  impacto: DS.attr('string'),
+  resultado_esperado: DS.attr('string'),
+  obj_estrategico: DS.attr('string'),
+  anio: DS.attr('number'),
+  fecha_inicio: DS.attr('date'),
+  fecha_termino: DS.attr('date'),
+  status: DS.attr('number'),
   solicitudes: DS.hasMany('solicitud', {async: true}),
 
   validations: {
