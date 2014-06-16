a4dc94d9c55b782d0c3cd6e6981349648802f922
Juan Calderon <juan.calderon@gmail.com>
2014-06-16 15:04:12 -0600

    Al guardar Solicitud, se actualiza solicitud_busqueda.

--- apps/vinculacion/app/assets/javascripts/vinculacion/controllers/solicitud/edit_controller.js
+++ apps/vinculacion/app/assets/javascripts/vinculacion/controllers/solicitud/edit_controller.js
@@ -1,11 +1,16 @@
 App.SolicitudEditController = Ember.ObjectController.extend({
-  needs: ['application'],
+  needs: ['application', 'solicitudes'],
   isNotDirty: Ember.computed.not('content.isDirty'),
   actions: {
     submit: function() {
       solicitud = this.get('model');
       self = this;
       var onSuccess = function(solicitud) {
+
+        // despues de guardar una nueva solicitud
+        // recarga la lista de solicitud_busqueda
+        self.get('controllers.solicitudes').send('reloadModel');
+
         self.transitionToRoute('solicitud', solicitud);
         self.get('controllers.application').notify('Se actualizó solicitud');
       };
