2544eee017a386d0ec5ebcf59736d482ef903a43
Juan Calderon <juan.calderon@gmail.com>
2014-07-16 12:58:46 -0600

    Si la cédula no ha recibido los costos variables, las tablas de costos y remanente aparecen opacas.

--- apps/vinculacion/app/assets/javascripts/vinculacion/controllers/cedula_controller.js
+++ apps/vinculacion/app/assets/javascripts/vinculacion/controllers/cedula_controller.js
@@ -9,9 +9,17 @@ App.CedulaController = Ember.ObjectController.extend({
     return result;
   }.property('model.porcentaje_participacion'),
 
-  chk_total_porcentaje : function() {
+  chk_total_porcentaje: function() {
     var result = this.get('total_porcentaje') == 100;
     return result;
-  }.property('total_porcentaje')
+  }.property('total_porcentaje'),
+
+  has_costos_class: function() {
+    var result = "";
+    if (this.get('model.costos_variables.length') == 0) {
+      result = "div-costos-variables-remanente-opacity";
+    }
+    return result;
+  }.property('model.costos_variables')
 
 });
