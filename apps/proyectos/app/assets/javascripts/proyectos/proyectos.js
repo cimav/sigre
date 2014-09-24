1f215d73d967e4bf608eb218936c67b986b42ee3
Jonathan Hernandez <ion@cimav.edu.mx>
2014-09-24 10:39:12 -0600

    Usar controlador que esta en proyectos (no proyecto)

--- apps/proyectos/app/assets/javascripts/proyectos/proyectos.js
+++ apps/proyectos/app/assets/javascripts/proyectos/proyectos.js
@@ -3,8 +3,6 @@
 //= require_tree ./models
 //= require_tree ./controllers
 //= require_tree ./views
-//= require_tree ./helpers
-//= require_tree ./components
 //= require_tree ./templates
 //= require ./router
 //= require_tree ./routes
