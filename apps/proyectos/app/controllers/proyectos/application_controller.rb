177eac5826dd111f6c072d9bde5f9348bcb590ab
Juan Calderon <juan.calderon@gmail.com>
2014-10-01 16:19:25 -0600

    Filtro de autentificación

--- apps/proyectos/app/controllers/proyectos/application_controller.rb
+++ apps/proyectos/app/controllers/proyectos/application_controller.rb
@@ -1,5 +1,31 @@
 module Proyectos
   class ApplicationController < ActionController::Base
+
+    def authenticated?
+      if session[:user_auth].blank?
+        user = Usuario.where(:email => session[:user_email], :status => Usuario::STATUS_ACTIVO).first
+        session[:user_auth] = user && user.email == session[:user_email]
+        if session[:user_auth]
+          session[:user_id] = user.id
+        end
+      else
+        session[:user_auth]
+      end
+    end
+    helper_method :authenticated?
+
+    def auth_required
+      redirect_to '/login' unless authenticated?
+    end
+
+    helper_method :current_user
+
+    private
+    def current_user
+      @current_user ||= Usuario.find(session[:user_id]) if session[:user_id]
+    end
+
+
   end
 end
 
--- apps/proyectos/app/controllers/proyectos/assets_controller.rb
+++ apps/proyectos/app/controllers/proyectos/assets_controller.rb
@@ -2,5 +2,10 @@ require_dependency "proyectos/application_controller"
 
 module Proyectos
   class AssetsController < ApplicationController
+
+    before_filter :auth_required
+    def index
+    end
+
   end
 end
