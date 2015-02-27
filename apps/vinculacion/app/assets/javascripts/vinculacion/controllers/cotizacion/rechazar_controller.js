f05f3356fb08c56d62482ba6b8126b9035a5dc97
Juan Calderon <juan.calderon@gmail.com>
2015-02-27 10:44:43 -0700

    Al rechazar Cotización le hace un reload a la Solicitud para crear la nueva cotiazción. Fix #23

--- apps/vinculacion/app/assets/javascripts/vinculacion/controllers/cotizacion/rechazar_controller.js
+++ apps/vinculacion/app/assets/javascripts/vinculacion/controllers/cotizacion/rechazar_controller.js
@@ -9,17 +9,21 @@ App.CotizacionRechazarController = Ember.ObjectController.extend({
 
       var onSuccess = function (cotizacion) {
         self.get('controllers.application').notify('Cotización ' + self.get('consecutivo') + ' rechazada.' );
-        
+
         // Cuando una solicitud se rechaza desde el backend se crea una nueva
         // cotización basada en la rechazada. Es por eso que transicionamos a esta.
-        ultimo = self.get('model').get('solicitud.cotizaciones.lastObject');
-        self.transitionToRoute('cotizacion', ultimo);
+        self.get('model').get('solicitud').reload().then(function (solicitud) {
+          var ultima = solicitud.get('cotizaciones.lastObject');
+          self.transitionToRoute('cotizacion', ultima);
+        });
+
       };
 
       var onFail = function (cotizacion) {
         self.get('controllers.application').notify('Error al re-cotizar', 'alert-danger');
       };
 
+      // al poner status=rechazado, el backend crea una nueva cotización
       cotizacion.set('status', self.get('controllers.cotizacion.Status.rechazado'));
       cotizacion.save().then(onSuccess, onFail);
     }
