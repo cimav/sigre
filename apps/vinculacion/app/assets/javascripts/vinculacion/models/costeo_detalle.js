f1470f9770e52a179e78b47aea58c0ae1841e6d9
Juan Calderon <juan.calderon@gmail.com>
2015-04-09 10:29:21 -0600

    Tipo II deshabilatado para pruebas de desarrollo.

--- apps/vinculacion/app/assets/javascripts/vinculacion/models/costeo_detalle.js
+++ apps/vinculacion/app/assets/javascripts/vinculacion/models/costeo_detalle.js
@@ -2,7 +2,7 @@ App.CosteoDetalle = DS.Model.extend({
   costeo:           DS.belongsTo('costeo'),
   tipo:             DS.attr('number'),
   descripcion:      DS.attr('string'),
-  cantidad:         DS.attr('string'),
-  precio_unitario:  DS.attr('string'),
+  cantidad:         DS.attr('number'),
+  precio_unitario:  DS.attr('number'),
   status:           DS.attr('number')
 });
