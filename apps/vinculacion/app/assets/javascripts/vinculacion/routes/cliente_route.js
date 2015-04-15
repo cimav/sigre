f45b4bac3887fa06df04b044887a3c8046b2f8d5
Juan Calderon <juan.calderon@gmail.com>
2015-04-15 10:07:31 -0600

    Carga Contactos cuando se le click al cliente ya seleccionado. Fix #30

--- apps/vinculacion/app/assets/javascripts/vinculacion/routes/cliente_route.js
+++ apps/vinculacion/app/assets/javascripts/vinculacion/routes/cliente_route.js
@@ -1,10 +1,7 @@
 App.ClienteRoute = Ember.Route.extend({
   afterModel: function () {
-
     // cada vez que se cambia de Cliente, redireccionarse/cargar la ruta Contactos
     this.transitionTo('contactos');
-    //TODO no funciona con el click sobre simismo.
-
   },
 
   beforeModel: function() {
--- apps/vinculacion/app/assets/javascripts/vinculacion/views/cliente_click_view.js
+++ apps/vinculacion/app/assets/javascripts/vinculacion/views/cliente_click_view.js
@@ -1,14 +1,9 @@
 App.ClienteClickView = Ember.View.extend({
-//    init: function() {
-//        this._super();
-//        this.set("controller", "cliente");
-//    },
 
   // Detecta el click sobre el Cliente para activar/mostrar sus Contactos
   // (Cliente.AfterModel no funciona cuando se selecciona a sí mismo.)
-    click: function(evt) {
-      // TODO Al darle Click al Cliente, debe re-cargar la ruta de contactos.
-      //console.log(App.controller);
-    }
+  click: function(evt) {
+      this.get('controller.controllers.cliente').transitionToRoute('contactos');
+  }
 
 });
