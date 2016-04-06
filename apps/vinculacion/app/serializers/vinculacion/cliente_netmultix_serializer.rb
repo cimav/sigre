c1733845495fcf2c713f218f2b13e95752784a9e
Juan Calderon <juan.calderon@gmail.com>
2016-04-06 14:05:00 -0600

    Corrección mínima.

--- apps/vinculacion/app/serializers/vinculacion/cliente_netmultix_serializer.rb
+++ apps/vinculacion/app/serializers/vinculacion/cliente_netmultix_serializer.rb
@@ -1,6 +1,6 @@
 module Vinculacion
   class ClienteNetmultixSerializer < ActiveModel::Serializer
-    attributes :id, :cl01_clave, :cl01_nombre, :cl01_rfc, :extra
+    attributes :id, :cl01_clave, :cl01_nombre, :cl01_rfc
     has_many :contactos_netmultix
   end
 end
