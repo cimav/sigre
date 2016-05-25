626411b1c0c24e3a161f78d1f8a2ffa0e1a823a3
JuanCalderon <Pest1lozzi>
2016-05-25 16:59:16 -0600

    Obteniendo datos para enviar a la cédula de netmultix

--- apps/vinculacion/app/models/vinculacion/cedula.rb
+++ apps/vinculacion/app/models/vinculacion/cedula.rb
@@ -65,6 +65,54 @@ module Vinculacion
       if self.status_changed? && self.status == TRANSMITIENDO
           self.status = TRANSMITIDA
 
+          cve_cliente_netmultix = self.cliente_netmultix_id.to_s.rjust(5,'0') rescue 'no-cve'
+          cliente_netmultix = ClienteNetmultix.where("cl01_clave LIKE '%" + cve_cliente_netmultix + "%'").first
+
+
+          cia = '1'
+          fecha = Time.now
+          orden_compra = self.solicitud.orden_compra rescue 'sin-orden'
+          sol_servicio = self.solicitud.codigo rescue 'sin-codigo|sol_servicio'
+          servicio = 201
+          tipo = 'S'
+          proyecto = '2033000238'
+          descripcion = self.servicio.nombre rescue 'sin-descripcion|nombre-servicio'
+          concepto_factura = self.concepto_en_extenso rescue 'sin-concepto-extenso'
+          cliente_netmultix_cve = cliente_netmultix.cl01_clave rescue 'sin-clave-cliente-net'
+          cliente_netmultix_nombre = cliente_netmultix.cl01_nombre rescue 'sin-nombre-cliente-net'
+          cliente_netmultix_rfc = cliente_netmultix.cl01_rfc rescue 'sin-rfc-cliente-net'
+
+          cliente_netmultix_calle = cliente_netmultix.cl01_calle rescue 'sin-calle-cliente-net'
+          cliente_netmultix_colonia = cliente_netmultix.cl01_colonia rescue 'sin-colonia-cliente-net'
+          cliente_netmultix_ciudad = cliente_netmultix.cl01_ciudad rescue 0
+          cliente_netmultix_postal = cliente_netmultix.cl01_postal rescue 'sin-postal-cliente-net'
+          cliente_netmultix_lada = cliente_netmultix.cl01_lada rescue 'sin-lada-cliente-net'
+          cliente_netmultix_telefono = cliente_netmultix.cl01_telefono1 rescue 'sin-tel1-cliente-net'
+          cliente_netmultix_fax = cliente_netmultix.cl01_fax rescue 'sin-fax-cliente-net'
+
+          requisitor = self.servicio.empleado.nombre_completo
+
+          puts cia
+          puts fecha
+          puts orden_compra
+          puts sol_servicio
+          puts servicio
+          puts tipo
+          puts proyecto
+          puts descripcion
+          puts concepto_factura
+          puts cliente_netmultix_cve
+          puts cliente_netmultix_nombre
+          puts cliente_netmultix_rfc
+          puts cliente_netmultix_calle
+          puts cliente_netmultix_colonia
+          puts cliente_netmultix_ciudad
+          puts cliente_netmultix_postal
+          puts cliente_netmultix_lada
+          puts cliente_netmultix_telefono
+          puts cliente_netmultix_fax
+          puts requisitor
+
           cedulaNetMultix = CedulaNetmultix.where('ft16_sol_servicio LIKE :q', {:q => '%999/17%'}).first
           puts cedulaNetMultix.ft16_cedula rescue 'no cedula'
           self.cedula_netmultix = cedulaNetMultix.ft16_cedula
