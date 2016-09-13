08a22dd55eaee0fd74face66b06c1081dab5c252
JuanCalderon <Pest1lozzi>
2016-09-13 10:59:39 -0600

    Corrección merge anterior.

--- apps/vinculacion/app/assets/javascripts/vinculacion/controllers/cedula_controller.js
+++ apps/vinculacion/app/assets/javascripts/vinculacion/controllers/cedula_controller.js
@@ -121,29 +121,6 @@ App.CedulaController = Ember.ObjectController.extend({
     }.property('model.status'),
 
 
-
-  Status: {
-    inicial: 1,
-    transmitiendo: 2,
-    transmitido: 3,
-    cancelada: 99
-  },
-
-  actions: {
-    transmitir: function (cedula) {
-
-    }
-  },
-
-  isEdicion: function() {
-    return this.get('model.status') == this.get('Status.inicial');
-  }.property('model.status'),
-
-  noPuedeTransmitir: function() {
-    // verificar que todos los campos esten validados
-    return false;
-  }.property('model.status'),
-
   total_porcentaje: function() {
     var remanentes = this.get('model.remanentes');
     var result = 0.00;
