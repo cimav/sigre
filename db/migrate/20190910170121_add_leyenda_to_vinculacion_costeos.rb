2178d8572cacb69eab57365b0323566f0a121351
JuanCalderon <Pest1lozzi>
2019-09-13 08:31:18 -0600

    mostrar_leyenda es true por default

--- db/migrate/20190910170121_add_leyenda_to_vinculacion_costeos.rb
+++ db/migrate/20190910170121_add_leyenda_to_vinculacion_costeos.rb
@@ -1,6 +1,6 @@
 class AddLeyendaToVinculacionCosteos < ActiveRecord::Migration
   def change
     add_column :vinculacion_costeos, :leyenda, :text
-    add_column :vinculacion_costeos, :mostrar_leyenda, :boolean, :default => false
+    add_column :vinculacion_costeos, :mostrar_leyenda, :boolean, :default => true
   end
 end
